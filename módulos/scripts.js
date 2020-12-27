aa50780c00fcf2f65d0cf09cc6a27069788afee9
messiashub <claudineimessias@hotmail.com>
2020-12-27 18:42:40 -0300

    update

---
+++ módulos/scripts.js
@@ -0,0 +1,19 @@
+let calc = require('./calculo')  // Importando o módulo
+
+let args = process.argv.slice(2);
+
+let a = Number(args[1]);
+let b = Number(args[2]);
+let c = "";
+
+if (args[0] == 's') {
+    c = calc.soma(a, b);
+} else if (args[0] == 'm') {
+    c = calc.mult(a, b);
+} else {
+    c = 'opção inválida';
+}
+
+
+console.log(c);
+
